090f10b247149110c2dc0bce47fe9d621dda0bd3
T Taylor <terrytaylorbonn@gmail.com>
2025-01-28 09:10:01 -0500

    connect to mongo ok

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 //server.js
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
 const corsOptions = {
     origin: 'http://localhost:5173' // only accept from this origin
 };
 app.use(cors(corsOptions));
+
+// Connect to MongoDB
+const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 app.get('/api', (req, res) => {
     res.json({fruits: ['aaaa', 'dddd', 'cccc']});
